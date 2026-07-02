9ad94cc8b6a9135c870b65a1a95e803aec0ab085
angelinalitvinova13-ux <angelinalitvinova13@gmail.com>
2026-07-03 02:27:38 +0400

    Update game.js

--- пробник пркатика/js/game.js
+++ пробник пркатика/js/game.js
@@ -1,3 +1,6 @@
+// Добавьте переменные камеры
+let camera = { x: 0, y: 0 };
+
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
@@ -6,23 +9,25 @@ canvas.height = window.innerHeight;
 
 const player = new Player();
 
-function gameLoop(){
+// В game.js
+let currentMap = new Map("assets/map/room1.png", 3000, 1080); 
 
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+function gameLoop() {
 
-    player.update();
+    player.update(currentMap);
 
-    player.draw(ctx);
+    camera.x = player.x - canvas.width / 2 + player.width / 2;
+    camera.y = player.y - canvas.height / 2 + player.height / 2;
+
+    camera.x = Math.max(0, Math.min(camera.x, currentMap.width - canvas.width));
+    camera.y = Math.max(0, Math.min(camera.y, currentMap.height - canvas.height));
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    currentMap.draw(ctx, camera);
+    player.draw(ctx, camera);
 
     requestAnimationFrame(gameLoop);
-
 }
 
 gameLoop();
-
-window.addEventListener("resize",()=>{
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-});
